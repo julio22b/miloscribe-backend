03a0f674cd314700058dc4619011d200666e2f4c
julio <jbermudezfc90@gmail.com>
2026-06-30 20:46:50 -0400

    add patch to cors

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ app.use(express.json());
 app.use(
     cors({
         origin,
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
         allowedHeaders: ['Content-Type', 'Authorization'],
         credentials: true,
     }),
